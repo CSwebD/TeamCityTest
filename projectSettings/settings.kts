52b71ac8b671d12e1c7ac45519ebfd21ac74ebbc
CSwebD <sclifos.cristian@gmail.com>
2025-01-26 21:42:59 +0000

    Update Kotlin DSL

--- projectSettings/settings.kts
+++ projectSettings/settings.kts
@@ -3,18 +3,22 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.nodeJS
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 project {
+    // Define the VCS root for the GitHub repository
     vcsRoot(HttpsGithubComCSwebDTeamCityTestGitRefsHeadsMain)
 
+    // Define the build configuration
     buildType(BuildAndTestWithoutDocker)
 }
 
 object BuildAndTestWithoutDocker : BuildType({
     name = "Build and Test Without Docker"
 
+    // Associate the VCS root with this build configuration
     vcs {
         root(HttpsGithubComCSwebDTeamCityTestGitRefsHeadsMain)
     }
 
+    // Define the build steps
     steps {
         nodeJS {
             name = "Install Dependencies"
@@ -35,5 +39,5 @@ object HttpsGithubComCSwebDTeamCityTestGitRefsHeadsMain : GitVcsRoot({
     name = "TeamCityTest Main Branch"
     url = "https://github.com/CSwebD/TeamCityTest.git"
     branch = "refs/heads/main"
-    branchSpec = "refs/heads/*"
+    branchSpec = "+:refs/heads/*"
 })
